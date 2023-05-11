f13bbae34242bea3f7815bb5e0c92fb7dd20ff6a
EnoDucotterd <eno.ducotterd@studentfr.ch>
2023-05-11 09:53:45 +0200

    ajout du js terminé

--- src/js/main.js
+++ src/js/main.js
@@ -8,12 +8,31 @@ var activateBurger = function () {
 
 trigger.addEventListener("click", activateBurger);
 
-var openMockup = document.querySelector(".product-range");
+var rotateBurger = document.querySelector(".header-burger");
 
-var slidder = document.querySelector(".button");
+var rotater = document.querySelector(".header-Burger");
 
-var activateMockup = function () {
-  openMockup.classList.toggle(".is-slipped");
+var activateRotate = function () {
+  rotateBurger.classList.toggle("is-rotated");
 };
 
-slidder.addEventListener("click", activateMockup);
+rotater.addEventListener("click", activateRotate);
+
+// var openMockups = document.querySelector(".product-range");
+
+// var reveal = document.querySelector(".button");
+
+// var mockupsrevealed = function () {
+//   openMockups.classList.toggle("is-revealed");
+// };
+
+// reveal.addEventListener("click", mockupsrevealed);
+
+var buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+  var id = button.dataset.products;
+  var productRange = document.getElementById(id);
+  button.addEventListener("click", () => {
+    productRange.classList.toggle("is-revealed");
+  });
+});
